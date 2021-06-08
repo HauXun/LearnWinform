165a1c57cd4ff650fe8661efd2bdb8aca5325649
Carl <hautrieu65@gmail.com>
2021-06-07 17:39:14 -0700

    Additional Ex

---
+++ KhoaDaoTao/Khoa-1/TextBoxGUI/TextBoxGUI/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace TextBoxGUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBoxValue = new System.Windows.Forms.TextBox();
+			this.labelValue = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(73, 40);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(137, 191);
+			this.textBox1.TabIndex = 3;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Enabled = false;
+			this.textBox2.Location = new System.Drawing.Point(313, 40);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.ReadOnly = true;
+			this.textBox2.RightToLeft = System.Windows.Forms.RightToLeft.No;
+			this.textBox2.Size = new System.Drawing.Size(135, 23);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.Text = "Tao bắn mày nhan mày";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(216, 250);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBoxValue
+			// 
+			this.textBoxValue.Location = new System.Drawing.Point(73, 250);
+			this.textBoxValue.Name = "textBoxValue";
+			this.textBoxValue.Size = new System.Drawing.Size(137, 23);
+			this.textBoxValue.TabIndex = 0;
+			this.textBoxValue.TextChanged += new System.EventHandler(this.textBoxValue_TextChanged);
+			// 
+			// labelValue
+			// 
+			this.labelValue.AutoSize = true;
+			this.labelValue.Location = new System.Drawing.Point(73, 293);
+			this.labelValue.Name = "labelValue";
+			this.labelValue.Size = new System.Drawing.Size(38, 15);
+			this.labelValue.TabIndex = 4;
+			this.labelValue.Text = "label1";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.CancelButton = this.button1;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.labelValue);
+			this.Controls.Add(this.textBoxValue);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBoxValue;
+		private System.Windows.Forms.Label labelValue;
+	}
+}
+
---
+++ KhoaDaoTao/Khoa-1/TextBoxGUI/TextBoxGUI/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBoxGUI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			int number = 0;
+			if (int.TryParse(textBoxValue.Text, out number))
+				labelValue.Text = Math.Pow(number, 2).ToString();
+			else
+				labelValue.Text = "Nhập số đi thằng Hai Zụ";
+		}
+
+		private void textBoxValue_TextChanged(object sender, EventArgs e)
+		{
+			int number = 0;
+			if (int.TryParse(textBoxValue.Text, out number))
+				labelValue.Text = Math.Pow(number, 2).ToString();
+			else
+				labelValue.Text = "Nhập số đi thằng Hai Zụ";
+		}
+	}
+}
