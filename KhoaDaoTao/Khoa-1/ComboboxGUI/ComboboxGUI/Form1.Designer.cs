613996c635c8652513bec3e4fd065b1ac3ab6783
Carl <hautrieu65@gmail.com>
2021-06-09 10:46:50 -0700

    Additional Ex

---
+++ KhoaDaoTao/Khoa-1/ComboboxGUI/ComboboxGUI/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace ComboboxGUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.comboBoxBranch = new System.Windows.Forms.ComboBox();
+			this.comboBoxCLass = new System.Windows.Forms.ComboBox();
+			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+			this.comboBox1.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems;
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(12, 42);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(333, 23);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(125, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Load List Items";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(382, 42);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 23);
+			this.textBox1.TabIndex = 2;
+			// 
+			// comboBoxBranch
+			// 
+			this.comboBoxBranch.FormattingEnabled = true;
+			this.comboBoxBranch.Location = new System.Drawing.Point(13, 141);
+			this.comboBoxBranch.Name = "comboBoxBranch";
+			this.comboBoxBranch.Size = new System.Drawing.Size(332, 23);
+			this.comboBoxBranch.TabIndex = 3;
+			this.comboBoxBranch.SelectedValueChanged += new System.EventHandler(this.comboBoxBranch_SelectedValueChanged);
+			// 
+			// comboBoxCLass
+			// 
+			this.comboBoxCLass.FormattingEnabled = true;
+			this.comboBoxCLass.Location = new System.Drawing.Point(382, 141);
+			this.comboBoxCLass.Name = "comboBoxCLass";
+			this.comboBoxCLass.Size = new System.Drawing.Size(337, 23);
+			this.comboBoxCLass.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.comboBoxCLass);
+			this.Controls.Add(this.comboBoxBranch);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.ComboBox comboBoxBranch;
+		private System.Windows.Forms.ComboBox comboBoxCLass;
+	}
+}
+
---
+++ KhoaDaoTao/Khoa-1/ComboboxGUI/ComboboxGUI/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboboxGUI
+{
+	public partial class Form1 : Form
+	{
+		List<VNCH> listItems;
+		List<CBClass> listClass;
+
+		public Form1()
+		{
+			InitializeComponent();
+			listItems = new List<VNCH>() {
+			new VNCH() {CongSan = "Hai Zụ", DanChu = "Trần Dần"},
+			new VNCH() { CongSan = "VnJat", DanChu = "Tôn" },
+			new VNCH() { CongSan = "SickDuck", DanChu = "SuckD*ck" }
+			};
+			comboBox1.DataSource = listItems;
+			comboBox1.DisplayMember = "CongSan";
+
+			AddBinding();
+
+			listClass = new List<CBClass>();
+			listClass.Add(
+				new CBClass()
+				{
+					ClassName = "CTK44A",
+					ListStudent = new List<string>()
+					{
+						"Hậu",
+						"Thắng",
+						"Trang",
+						"Vỹ"
+					}
+				}
+				);
+			listClass.Add(
+				new CBClass()
+				{
+					ClassName = "CTK44B",
+					ListStudent = new List<string>()
+					{
+						"Yến",
+						"Sơn",
+						"Long",
+						"Sang"
+					}
+				}
+				);
+			comboBoxBranch.DataSource = listClass;
+			comboBoxBranch.DisplayMember = "ClassName";
+			//AddClassBinding();
+		}
+
+		void AddClassBinding()
+		{
+			comboBoxCLass.DataBindings.Add("DataSource", comboBoxBranch.SelectedValue, "ListStudent",
+				true, DataSourceUpdateMode.OnPropertyChanged);
+		}
+
+		void AddBinding()
+		{
+			textBox1.DataBindings.Add(new Binding("Text", comboBox1.DataSource, "DanChu"));
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			//ComboBox comboBox = sender as ComboBox;
+
+			//if (comboBox.SelectedValue != null)
+			//{
+			//	VNCH vnch = comboBox.SelectedValue as VNCH;
+			//	textBox1.Text = vnch.DanChu;
+			//}
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			//listItems = new List<VNCH>() {
+			//new VNCH() {CongSan = "Hai Zụ", DanChu = "Trần Dần"},
+			//new VNCH() { CongSan = "VnJat", DanChu = "Tôn" },
+			//new VNCH() { CongSan = "SickDuck", DanChu = "SuckD*ck" }
+			//};
+			//comboBox1.DataSource = listItems;
+			//comboBox1.DisplayMember = "CongSan";
+		}
+
+		private void comboBoxBranch_SelectedValueChanged(object sender, EventArgs e)
+		{
+			ComboBox comboBox = sender as ComboBox;
+
+			if (comboBox.SelectedValue != null)
+			{
+				CBClass @class = comboBox.SelectedValue as CBClass;
+				comboBoxCLass.DataSource = @class.ListStudent;
+				//	comboBoxCLass.DisplayMember = "ListStudent";
+			}
+		}
+	}
+
+	class VNCH
+	{
+		public string CongSan { get; set; }
+		public string DanChu { get; set; }
+	}
+	
+	class CBClass
+	{
+		public string ClassName { get; set; }
+		public List<string> ListStudent { get; set; }
+	}
+}
