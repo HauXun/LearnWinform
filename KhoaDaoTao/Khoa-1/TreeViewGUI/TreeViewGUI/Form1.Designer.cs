1f76ab4fadbe7469c4036d344c4724e4ee44f251
Carl <hautrieu65@gmail.com>
2021-06-09 20:00:07 -0700

    Additional Ex

---
+++ KhoaDaoTao/Khoa-1/TreeViewGUI/TreeViewGUI/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿
+namespace TreeViewGUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.treeViewShow = new System.Windows.Forms.TreeView();
+			this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+			this.SuspendLayout();
+			// 
+			// treeViewShow
+			// 
+			this.treeViewShow.ImageIndex = 0;
+			this.treeViewShow.ImageList = this.imageList1;
+			this.treeViewShow.Location = new System.Drawing.Point(12, 12);
+			this.treeViewShow.Name = "treeViewShow";
+			this.treeViewShow.SelectedImageIndex = 0;
+			this.treeViewShow.Size = new System.Drawing.Size(626, 426);
+			this.treeViewShow.TabIndex = 0;
+			// 
+			// imageList1
+			// 
+			this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+			this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+			this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+			this.imageList1.Images.SetKeyName(0, "15yearsdefqon.png");
+			this.imageList1.Images.SetKeyName(1, "SundayFunday.png");
+			this.imageList1.Images.SetKeyName(2, "TheClosingCeremory.jpeg");
+			this.imageList1.Images.SetKeyName(3, "PowerHour.png");
+			this.imageList1.Images.SetKeyName(4, "TheCamping.png");
+			this.imageList1.Images.SetKeyName(5, "TheGathering.png");
+			this.imageList1.Images.SetKeyName(6, "WastedLand.PNG");
+			this.imageList1.Images.SetKeyName(7, "TheEndshow.PNG");
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.treeViewShow);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TreeView treeViewShow;
+		private System.Windows.Forms.ImageList imageList1;
+	}
+}
+
---
+++ KhoaDaoTao/Khoa-1/TreeViewGUI/TreeViewGUI/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TreeViewGUI
+{
+	public partial class Form1 : Form
+	{
+		string path = @"E:\";
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			//LoadTreeView();
+
+			if (Directory.Exists(path))
+			{
+				TreeNode root = new TreeNode() { Text = path};
+				treeViewShow.Nodes.Add(root);
+				LoadExplorer(root);
+			}	
+		}
+
+		void LoadExplorer(TreeNode root)
+		{
+			if (root == null)
+				return;
+
+			try
+			{
+
+				var folderList = new DirectoryInfo(root.Text).GetDirectories();
+
+				if (folderList.Count() == 0)
+					return;
+
+				foreach (DirectoryInfo item in folderList)
+				{
+					if (Directory.Exists(item.FullName))
+					{
+						TreeNode nodeRoot = new TreeNode() { Text = item.FullName };
+						root.Nodes.Add(nodeRoot);
+						LoadExplorer(nodeRoot);
+					}
+				}
+			}
+			catch (Exception)
+			{
+				return;
+			}
+		}
+		
+		void LoadTreeView()
+		{
+			treeViewShow.CheckBoxes = true;
+			treeViewShow.NodeMouseClick += TreeViewShow_NodeMouseClick;
+
+			TreeNode root = new TreeNode();
+			root.Text = "Hai Zụ";
+			root.ImageIndex = 0;
+
+			TreeNode nodeRoot = new TreeNode() { Text = "Chửi"};
+			root.Nodes.Add(nodeRoot);
+			nodeRoot.ImageIndex = 1;
+			nodeRoot.Checked = true;
+
+			TreeNode root2 = new TreeNode();
+			root2.Text = "Chần Dần";
+			root2.ImageIndex = 2;
+
+			TreeNode nodeRoot2 = new TreeNode() { Text = "Có súng đây nè" };
+			root2.Nodes.Add(nodeRoot2);
+			nodeRoot2.ImageIndex = 3;
+
+			treeViewShow.Nodes.Add(root);
+			treeViewShow.Nodes.Add(root2);
+		}
+
+		private void TreeViewShow_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
+		{
+			//MessageBox.Show(e.Node.Text);
+			e.Node.Checked = !e.Node.Checked;
+		}
+	}
+}
