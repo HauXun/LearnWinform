f39e257a6cbca1c5e916f02a71e9415605770cf9
HauXun <hautrieu65@gmail.com>
2021-06-13 13:46:31 -0700

    Additional Ex

---
+++ KhoaDaoTao/Khoa-1/ThreadGUI/ThreadGUI/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace ThreadGUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.Left = new System.Windows.Forms.CheckBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(199, 9);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(83, 9);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 23);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(65, 25);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "label1";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(280, 9);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 3;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::ThreadGUI.Properties.Resources._15yearsdefqon;
+			this.pictureBox1.Location = new System.Drawing.Point(353, 109);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 4;
+			this.pictureBox1.TabStop = false;
+			// 
+			// Left
+			// 
+			this.Left.AutoSize = true;
+			this.Left.Location = new System.Drawing.Point(191, 62);
+			this.Left.Name = "Left";
+			this.Left.Size = new System.Drawing.Size(46, 19);
+			this.Left.TabIndex = 5;
+			this.Left.Text = "Left";
+			this.Left.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(353, 57);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(243, 58);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(100, 23);
+			this.textBox2.TabIndex = 8;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.Left);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.CheckBox Left;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TextBox textBox2;
+	}
+}
+
---
+++ KhoaDaoTao/Khoa-1/ThreadGUI/ThreadGUI/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreadGUI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			Control.CheckForIllegalCrossThreadCalls = false;
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			ThreadStart threadStart = new ThreadStart(Demo);
+			Thread thread = new Thread(threadStart);
+			thread.IsBackground = true;
+			thread.Start();
+		}
+
+		void Demo()
+		{
+			for (int i = 0; i < int.Parse(textBox1.Text); i++)
+			{
+				label1.Text = i.ToString();
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			MessageBox.Show("Có súng đây nè!");
+		}
+
+		int speed = 100;
+		void MovePictureBox()
+		{
+			new Thread(
+			() =>
+			{
+				while (true)
+				{
+					if (Left.Checked)
+					{
+						pictureBox1.Location = new Point(pictureBox1.Location.X - 1, pictureBox1.Location.Y);
+					}
+					else
+					{
+						pictureBox1.Location = new Point(pictureBox1.Location.X + 1, pictureBox1.Location.Y);
+					}
+					Thread.Sleep(speed);
+				}
+			}
+			)
+			{ IsBackground = true }.Start();
+		}
+
+		private void button3_Click(object sender, EventArgs e)
+		{
+			MovePictureBox();
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+			if (int.TryParse(textBox2.Text, out speed))
+			{
+
+			}
+		}
+	}
+}
