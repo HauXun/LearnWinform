46cb373da8ea9e919ed17f436b57d3c62742f416
HauXun <hautrieu65@gmail.com>
2021-06-13 10:00:41 -0700

    Additional Ex

---
+++ KhoaDaoTao/Khoa-1/ToolTipGUI/ToolTipGUI/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToolTipGUI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			ToolTip toolTip = new ToolTip() {
+				InitialDelay = 0,
+				IsBalloon = true,
+				ToolTipIcon = ToolTipIcon.Error,
+				ToolTipTitle = "???"
+			};
+			toolTip.SetToolTip(button2, "Có súng đây nè!");
+		}
+	}
+}
