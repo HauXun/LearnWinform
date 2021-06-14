4e9e1a2afa315991574cfe6d6daf17fd6e74c056
HauXun <hautrieu65@gmail.com>
2021-06-13 19:15:15 -0700

    Additional Ex

---
+++ KhoaDaoTao/Khoa-1/TimerGUI/TimerGUI/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace TimerGUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.buttonStart = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+			this.label1.Location = new System.Drawing.Point(364, 184);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(28, 32);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "0";
+			// 
+			// buttonStart
+			// 
+			this.buttonStart.Location = new System.Drawing.Point(340, 158);
+			this.buttonStart.Name = "buttonStart";
+			this.buttonStart.Size = new System.Drawing.Size(75, 23);
+			this.buttonStart.TabIndex = 1;
+			this.buttonStart.Text = "Start";
+			this.buttonStart.UseVisualStyleBackColor = true;
+			this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.buttonStart);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button buttonStart;
+	}
+}
+
---
+++ KhoaDaoTao/Khoa-1/TimerGUI/TimerGUI/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TimerGUI
+{
+	public partial class Form1 : Form
+	{
+		Timer timer1;
+		public Form1()
+		{
+			InitializeComponent();
+			timer1 = new Timer();
+			timer1.Tick += timer1_Tick;
+			timer1.Interval = 1000;
+		}
+
+		private void buttonStart_Click(object sender, EventArgs e)
+		{
+			timer1.Enabled = !timer1.Enabled;
+			buttonStart.Text = buttonStart.Text == "Start" ? "Start" : "Stop";
+
+			//if (!timer1.Enabled)
+			//{
+			//	buttonStart.Text = "Stop";
+			//	timer1.Start();
+			//}
+			//else
+			//{
+			//	buttonStart.Text = "Start";
+			//	timer1.Stop();
+			//}
+		}
+
+		int i = 0;
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			i++;
+			label1.Text = i.ToString();
+		}
+	}
+}
