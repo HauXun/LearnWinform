f289d0d3d87836e4b1d0892d9ba38303015775f5
HauXun <hautrieu65@gmail.com>
2021-06-18 12:25:40 -0700

    Additional Ex

---
+++ KhoaDaoTao/Khoa-3/TextBox-ComboBox-Button/TextBox-ComboBox-Button/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace TextBox_ComboBox_Button
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.labelName = new System.Windows.Forms.Label();
+			this.textBoxName = new System.Windows.Forms.TextBox();
+			this.labelDay = new System.Windows.Forms.Label();
+			this.labelMonth = new System.Windows.Forms.Label();
+			this.labelYear = new System.Windows.Forms.Label();
+			this.comboBoxDay = new System.Windows.Forms.ComboBox();
+			this.comboBoxMonth = new System.Windows.Forms.ComboBox();
+			this.comboBoxYear = new System.Windows.Forms.ComboBox();
+			this.buttonCheck = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// labelName
+			// 
+			this.labelName.AutoSize = true;
+			this.labelName.Location = new System.Drawing.Point(13, 13);
+			this.labelName.Name = "labelName";
+			this.labelName.Size = new System.Drawing.Size(46, 15);
+			this.labelName.TabIndex = 0;
+			this.labelName.Text = "Họ tên:";
+			// 
+			// textBoxName
+			// 
+			this.textBoxName.Location = new System.Drawing.Point(65, 10);
+			this.textBoxName.Name = "textBoxName";
+			this.textBoxName.Size = new System.Drawing.Size(222, 23);
+			this.textBoxName.TabIndex = 1;
+			// 
+			// labelDay
+			// 
+			this.labelDay.AutoSize = true;
+			this.labelDay.Location = new System.Drawing.Point(13, 53);
+			this.labelDay.Name = "labelDay";
+			this.labelDay.Size = new System.Drawing.Size(38, 15);
+			this.labelDay.TabIndex = 2;
+			this.labelDay.Text = "Ngày:";
+			// 
+			// labelMonth
+			// 
+			this.labelMonth.AutoSize = true;
+			this.labelMonth.Location = new System.Drawing.Point(13, 94);
+			this.labelMonth.Name = "labelMonth";
+			this.labelMonth.Size = new System.Drawing.Size(43, 15);
+			this.labelMonth.TabIndex = 3;
+			this.labelMonth.Text = "Tháng:";
+			// 
+			// labelYear
+			// 
+			this.labelYear.AutoSize = true;
+			this.labelYear.Location = new System.Drawing.Point(13, 134);
+			this.labelYear.Name = "labelYear";
+			this.labelYear.Size = new System.Drawing.Size(36, 15);
+			this.labelYear.TabIndex = 4;
+			this.labelYear.Text = "Năm:";
+			// 
+			// comboBoxDay
+			// 
+			this.comboBoxDay.FormattingEnabled = true;
+			this.comboBoxDay.Location = new System.Drawing.Point(65, 45);
+			this.comboBoxDay.Name = "comboBoxDay";
+			this.comboBoxDay.Size = new System.Drawing.Size(121, 23);
+			this.comboBoxDay.TabIndex = 5;
+			// 
+			// comboBoxMonth
+			// 
+			this.comboBoxMonth.FormattingEnabled = true;
+			this.comboBoxMonth.Location = new System.Drawing.Point(65, 91);
+			this.comboBoxMonth.Name = "comboBoxMonth";
+			this.comboBoxMonth.Size = new System.Drawing.Size(121, 23);
+			this.comboBoxMonth.TabIndex = 6;
+			// 
+			// comboBoxYear
+			// 
+			this.comboBoxYear.FormattingEnabled = true;
+			this.comboBoxYear.Location = new System.Drawing.Point(65, 131);
+			this.comboBoxYear.Name = "comboBoxYear";
+			this.comboBoxYear.Size = new System.Drawing.Size(121, 23);
+			this.comboBoxYear.TabIndex = 7;
+			// 
+			// buttonCheck
+			// 
+			this.buttonCheck.Location = new System.Drawing.Point(212, 90);
+			this.buttonCheck.Name = "buttonCheck";
+			this.buttonCheck.Size = new System.Drawing.Size(75, 23);
+			this.buttonCheck.TabIndex = 8;
+			this.buttonCheck.Text = "&Check";
+			this.buttonCheck.UseVisualStyleBackColor = true;
+			this.buttonCheck.Click += new System.EventHandler(this.buttonCheck_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.buttonCheck);
+			this.Controls.Add(this.comboBoxYear);
+			this.Controls.Add(this.comboBoxMonth);
+			this.Controls.Add(this.comboBoxDay);
+			this.Controls.Add(this.labelYear);
+			this.Controls.Add(this.labelMonth);
+			this.Controls.Add(this.labelDay);
+			this.Controls.Add(this.textBoxName);
+			this.Controls.Add(this.labelName);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelName;
+		private System.Windows.Forms.TextBox textBoxName;
+		private System.Windows.Forms.Label labelDay;
+		private System.Windows.Forms.Label labelMonth;
+		private System.Windows.Forms.Label labelYear;
+		private System.Windows.Forms.ComboBox comboBoxDay;
+		private System.Windows.Forms.ComboBox comboBoxMonth;
+		private System.Windows.Forms.ComboBox comboBoxYear;
+		private System.Windows.Forms.Button buttonCheck;
+	}
+}
+
---
+++ KhoaDaoTao/Khoa-3/TextBox-ComboBox-Button/TextBox-ComboBox-Button/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TextBox_ComboBox_Button
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			for (int i = 1; i <= 31; i++)
+			{
+				comboBoxDay.Items.Add(i);
+			}
+			comboBoxDay.SelectedIndex = 0;
+
+			for (int i = 1; i <= 12; i++)
+			{
+				comboBoxMonth.Items.Add(i);
+			}
+			comboBoxMonth.SelectedIndex = 0;
+
+			for (int i = 1930; i <= 2010; i++)
+			{
+				comboBoxYear.Items.Add(i);
+			}
+			comboBoxYear.SelectedIndex = 0;
+		}
+
+		private void buttonCheck_Click(object sender, EventArgs e)
+		{
+			// Tháng sinh hợp lệ có 30 ngày: 4 - 6 - 9 - 11
+			// Năm không nhuận có 28 ngày
+			// Còn lại 31 ngày
+
+			byte ngay = byte.Parse(comboBoxDay.SelectedItem.ToString());
+			byte thang = byte.Parse(comboBoxMonth.SelectedItem.ToString());
+			int nam = int.Parse(comboBoxYear.SelectedItem.ToString());
+
+			switch (thang)
+			{
+				case 4:
+				case 6:
+				case 9:
+				case 11:
+					if (ngay > 30)
+					{
+						MessageBox.Show("Ngày sinh không hợp lệ!", "Lỗi Tháng!",
+							MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					}
+					else
+					{
+						MessageBox.Show($"{textBoxName.Text}: {ngay}/{thang}/{nam} hợp lệ!",
+							"Kiểm tra năm sinh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					}
+					break;
+				case 2:
+					if (((nam % 4 == 0) && (nam % 100 != 0)) || nam % 400 == 0)
+					{
+						// Đây là năm nhuận
+						if (ngay > 29)
+						{
+							MessageBox.Show("Ngày sinh không hợp lệ!", "Lỗi Năm Nhuận!",
+								MessageBoxButtons.OK, MessageBoxIcon.Warning);
+						}
+						else
+						{
+							MessageBox.Show($"{textBoxName.Text}: {ngay}/{thang}/{nam} hợp lệ!",
+								"Kiểm tra năm sinh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+						}
+					}
+					else
+					{
+						if (ngay > 28)
+						{
+							MessageBox.Show("Ngày sinh không hợp lệ!", "Lỗi Tháng!",
+								MessageBoxButtons.OK, MessageBoxIcon.Warning);
+						}
+						else
+						{
+							MessageBox.Show($"{textBoxName.Text}: {ngay}/{thang}/{nam} hợp lệ!",
+								"Kiểm tra năm sinh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+						}
+					}
+					break;
+				default:
+					if (ngay > 31)
+					{
+						MessageBox.Show("Ngày sinh không hợp lệ!", "Lỗi Ngày!",
+								MessageBoxButtons.OK, MessageBoxIcon.Warning);
+					}
+					else
+					{
+						MessageBox.Show($"{textBoxName.Text}: {ngay}/{thang}/{nam} hợp lệ!",
+							"Kiểm tra năm sinh", MessageBoxButtons.OK, MessageBoxIcon.Information);
+					}
+					break;
+			}
+		}
+	}
+}
