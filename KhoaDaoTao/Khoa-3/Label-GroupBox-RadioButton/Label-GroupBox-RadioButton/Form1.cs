88e7db616b98f617293c77fea44204ef0f05aed6
HauXun <hautrieu65@gmail.com>
2021-06-17 21:15:48 -0700

    Modified

--- KhoaDaoTao/Khoa-3/Label-GroupBox-RadioButton/Label-GroupBox-RadioButton/Form1.cs
+++ KhoaDaoTao/Khoa-3/Label-GroupBox-RadioButton/Label-GroupBox-RadioButton/Form1.cs
@@ -12,10 +12,18 @@ namespace Label_GroupBox_RadioButton
 {
 	public partial class Form1 : Form
 	{
+		FontDialog fontDlg = new FontDialog();
 		public Form1()
 		{
 			InitializeComponent();
+
 		}
+
+		void DefaultFont()
+		{
+			fontDlg.Font = new Font("Segoe UI", 9, FontStyle.Strikeout);
+		}
+
 		private void Form1_Load(object sender, EventArgs e)
 		{
 			foreach (Control c in groupBox1.Controls)
@@ -26,26 +34,44 @@ namespace Label_GroupBox_RadioButton
 				}
 			}
 		}
+
+		Label tempLabel;
 		private void Form1_CheckedChanged(object sender, EventArgs e)
 		{
-			var buttonsChecked = groupBox1.Controls.OfType<RadioButton>()
-			   .FirstOrDefault(a => a.Checked).Text;
+			RadioButton radioButton = sender as RadioButton;
 
-			labelResult.Text = buttonsChecked + ": ";
-			switch (buttonsChecked)
+			if (radioButton.Checked)
 			{
-				case "Chữ nghiêng":
-					label2.Font = new Font(label2.Font.Name, label2.Font.Size, FontStyle.Italic);
-					return;
-				case "Chữ Đậm":
-					label2.Font = new Font(label2.Font.Name, label2.Font.Size, FontStyle.Bold);
-					return;
-				case "Chữ In Hoa":
-					label2.Text = label2.Text.ToUpper();
-					return;
-				case "Xuống dòng":
-					label2.Text += "\n - ";
-					return;
+				if (tempLabel == null)
+				{
+					tempLabel = new Label();
+					tempLabel.Text = label2.Text;
+					tempLabel.Font = new Font(tempLabel.Font, FontStyle.Regular);
+				}
+				label2.Text = tempLabel.Text;
+				label2.Font = tempLabel.Font;
+
+				var buttonsChecked = groupBox1.Controls.OfType<RadioButton>()
+				   .FirstOrDefault(a => a.Checked).Text;
+
+				labelResult.Text = buttonsChecked + ": ";
+				switch (buttonsChecked)
+				{
+					case "Chữ nghiêng":
+						label2.Font = new Font(label2.Font.Name, label2.Font.Size, FontStyle.Italic);
+						break;
+					case "Chữ Đậm":
+						label2.Font = new Font(label2.Font.Name, label2.Font.Size, FontStyle.Bold);
+						break;
+					case "Chữ In Hoa":
+						label2.Text = label2.Text.ToUpper();
+						break;
+					case "Xuống dòng":
+						label2.Text = tempLabel.Text;
+						label2.Text += "\n - ";
+						break;
+				}
+				radioButton.Checked = !radioButton.Checked;
 			}
 		}
 	}
