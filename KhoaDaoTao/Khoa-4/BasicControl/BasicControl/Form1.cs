117fffd79fbee513245a80c4c6d1ffe69a59572c
HauXun <hautrieu65@gmail.com>
2021-06-19 16:29:52 -0700

    Init Commit 🚀🚀🚀

---
+++ KhoaDaoTao/Khoa-4/BasicControl/BasicControl/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicControl
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_MouseEnter(object sender, EventArgs e)
+		{
+			Button button = sender as Button;
+			button.BackColor = Color.Black;
+		}
+
+		private void button1_MouseLeave(object sender, EventArgs e)
+		{
+			Button button = sender as Button;
+			button.BackColor = Color.FromArgb(250, 255, 255);
+		}
+
+		private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (char.IsDigit(e.KeyChar))
+			{
+				e.Handled = true;
+			}	
+		}
+	}
+}
