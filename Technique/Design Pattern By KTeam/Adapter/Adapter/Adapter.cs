2a984cde64886d0f4aff0e62a82e5cec8c85d534
HauXun <hautrieu65@gmail.com>
2021-06-26 09:49:26 -0700

    Additional Adapter Technique

---
+++ Technique/Design Pattern By KTeam/Adapter/Adapter/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+	class Adapter
+	{
+		Core core = new Core();
+		
+		public List<string> ChuanHoaDanhSachChuoi(List<String> danhSach)
+		{
+			List<string> result = new List<string>();
+			foreach (string item in danhSach)
+			{
+				result.Add(core.ChuanHoaChuoi(item));
+			}
+			return result;
+		}
+	}
+}
---
+++ Technique/Design Pattern By KTeam/Singleton/Singleton/SingletonClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Singleton
+{
+	class SingletonClass
+	{
+		private static volatile object key = new object();	// volatile: Tránh đụng độ Thread
+		private static SingletonClass instance;
+		public static SingletonClass Instance
+		{
+			get
+			{
+				if (instance == null)
+				{
+					lock (key)
+					{
+						instance = new SingletonClass();
+					}
+				}
+				return instance;
+			}
+			set => instance = value;
+		}
+
+		private SingletonClass()
+		{
+
+		}
+
+		public void Demo() => MessageBox.Show("Có súng đây nè!");
+
+		public void PrintString(string s) => MessageBox.Show(s);
+	}
+}
