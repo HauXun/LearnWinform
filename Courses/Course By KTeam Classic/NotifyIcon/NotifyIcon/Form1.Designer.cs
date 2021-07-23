7bde95e7ce3ed9b11523bb3eec6d6827baf61c3d
HauXun <hautrieu65@gmail.com>
2021-07-23 18:59:04 +0700

    Additional NotifyIcon Technique

---
+++ Courses/Course By KTeam Classic/NotifyIcon/NotifyIcon/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace NotifyIcon
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.nudHour = new System.Windows.Forms.NumericUpDown();
+			this.btnMessage = new System.Windows.Forms.Button();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.maximizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.minimizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+			this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+			((System.ComponentModel.ISupportInitialize)(this.nudHour)).BeginInit();
+			this.contextMenuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// nudHour
+			// 
+			this.nudHour.Location = new System.Drawing.Point(12, 12);
+			this.nudHour.Name = "nudHour";
+			this.nudHour.Size = new System.Drawing.Size(54, 23);
+			this.nudHour.TabIndex = 0;
+			this.nudHour.ValueChanged += new System.EventHandler(this.nudHour_ValueChanged);
+			// 
+			// btnMessage
+			// 
+			this.btnMessage.AutoSize = true;
+			this.btnMessage.Location = new System.Drawing.Point(91, 9);
+			this.btnMessage.Name = "btnMessage";
+			this.btnMessage.Size = new System.Drawing.Size(126, 25);
+			this.btnMessage.TabIndex = 1;
+			this.btnMessage.Text = "Show Value Message";
+			this.btnMessage.UseVisualStyleBackColor = true;
+			this.btnMessage.Click += new System.EventHandler(this.btnMessage_Click);
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "notifyIcon1";
+			this.notifyIcon1.Visible = true;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.maximizeToolStripMenuItem,
+            this.minimizeToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.toolStripTextBox1});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(181, 123);
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.exitToolStripMenuItem.Text = "&Exit";
+			this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+			// 
+			// maximizeToolStripMenuItem
+			// 
+			this.maximizeToolStripMenuItem.Name = "maximizeToolStripMenuItem";
+			this.maximizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.maximizeToolStripMenuItem.Text = "Maximize";
+			this.maximizeToolStripMenuItem.Click += new System.EventHandler(this.maximizeToolStripMenuItem_Click);
+			// 
+			// minimizeToolStripMenuItem
+			// 
+			this.minimizeToolStripMenuItem.Name = "minimizeToolStripMenuItem";
+			this.minimizeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.minimizeToolStripMenuItem.Text = "Minimize";
+			this.minimizeToolStripMenuItem.Click += new System.EventHandler(this.minimizeToolStripMenuItem_Click);
+			// 
+			// toolStripSeparator1
+			// 
+			this.toolStripSeparator1.Name = "toolStripSeparator1";
+			this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
+			// 
+			// toolStripTextBox1
+			// 
+			this.toolStripTextBox1.Name = "toolStripTextBox1";
+			this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+			this.toolStripTextBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.toolStripTextBox1_KeyDown);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(332, 143);
+			this.Controls.Add(this.btnMessage);
+			this.Controls.Add(this.nudHour);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			((System.ComponentModel.ISupportInitialize)(this.nudHour)).EndInit();
+			this.contextMenuStrip1.ResumeLayout(false);
+			this.contextMenuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NumericUpDown nudHour;
+		private System.Windows.Forms.Button btnMessage;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem maximizeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem minimizeToolStripMenuItem;
+		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+		private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+	}
+}
+
---
+++ Courses/Course By KTeam Classic/NotifyIcon/NotifyIcon/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotifyIcon
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void nudHour_ValueChanged(object sender, EventArgs e)
+		{
+			NumericUpDown numeric = sender as NumericUpDown;
+			if (numeric.Value >= 24)
+			{
+				numeric.Value = 0;
+			}
+			if (numeric.Value < 0)
+			{
+				numeric.Value = 23;
+			}
+		}
+
+		private void btnMessage_Click(object sender, EventArgs e)
+		{
+			ShowMessage(nudHour.Value.ToString());
+		}
+
+		void ShowMessage(string s)
+		{
+			MessageBox.Show(s);
+		}
+
+		private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void maximizeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Maximized;
+		}
+
+		private void minimizeToolStripMenuItem_Click(object sender, EventArgs e)
+		{
+			this.WindowState = FormWindowState.Minimized;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			Binding binding = new Binding("Value", toolStripTextBox1, "Text", true, DataSourceUpdateMode.OnPropertyChanged);
+			nudHour.DataBindings.Add(binding);
+		}
+
+		private void toolStripTextBox1_KeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				btnMessage_Click(this, new EventArgs());
+			}
+		}
+	}
+}
